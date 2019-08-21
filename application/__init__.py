3696325dca2597dbcdf3c594c5689aadd0685125
jaysaulhowarth <jayhowarthmbro@gmail.com>
2019-08-21 15:16:20 +0100

    Added Login Functonality

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +10,7 @@ app.config['SECRET_KEY'] = '1289f249a1e29403e0cbbe3656ce4569'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import Users
 
@@ -54,3 +54,19 @@ class RegistrationForm(FlaskForm):
 		user = Users.query.filter_by(email=email.data).first()
 		if user:
 			raise ValidationError('Email already in use!')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email',
+		validators=[
+			DataRequired(),
+			Email()
+		])
+
+	password = PasswordField('Password',
+		validators=[
+			DataRequired()
+		])
+
+	remember = BooleanField('Remember Me')
+
+	submit = SubmitField('Login')
