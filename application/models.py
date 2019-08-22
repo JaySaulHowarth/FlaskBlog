ee266bab0f3602e38e44382185b29329f832545a
jaysaulhowarth <jayhowarthmbro@gmail.com>
2019-08-22 11:01:50 +0100

    Added submit to login form

--- application/models.py
+++ application/models.py
@@ -6,7 +6,7 @@ class Posts(db.Model):
 	first_name = db.Column(db.String(30), nullable=False)
 	last_name = db.Column(db.String(30), nullable=False)
 	title = db.Column(db.String(100), nullable=False, unique=True)
-	content = db.Column(db.String(10000), nullable=False, unique=True)
+	content = db.Column(db.String(3000), nullable=False, unique=True)
 
 	def __repr__(self):
 		return ''.join([
@@ -16,7 +16,7 @@ class Posts(db.Model):
 class Users(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(150), nullable=False, unique=True)
-	password = db.Column(db.String(50), nullable=False)
+	password = db.Column(db.String(128), nullable=False)
 	
 	def __repr__(self):
 		return ''.join(['User ID: ', str(self.id), '\r\n', 'Email: ', self.email])
